e8c9417205127501a23e275cb6f12b0af560232a
Tammy Holt <tammyholt@Tammys-iMac.local>
2015-07-31 18:41:00 -0700

    More rand work

--- src/rand_range_int.cpp
+++ src/rand_range_int.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int RandRangeInt(int min, int max) {
-  return (int)floor(min + double(rand()*(max - min)) / double(RAND_MAX + 1.0));
+	return min + ((long int)rand())*(max-min) / (RAND_MAX+1.0);
 }
 
 int RandRangeInt(int max)
 {
-	return (int)floor(double(rand()*max) / double((RAND_MAX + 1.0)));
+	return ((long int)rand())*max / (RAND_MAX+1.0);
 }
